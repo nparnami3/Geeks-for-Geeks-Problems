6c57c84ae2230ab838aad5bf8fd7321a1f2a5e5a
Nishtha Parnami <87017035+nparnami3@users.noreply.github.com>
2025-04-19 09:39:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/At Least K Occurrences/at-least-k-occurrences.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int firstElementKTime(vector<int>& arr, int k) {
+        // write code
+        unordered_map<int,int>count;
+        // insert the frequency
+        for(auto num : arr){
+            count[num]++;
+            if(count[num] == k) return num;
+        }
+        return -1;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution obj;
+        int res = obj.firstElementKTime(arr, k);
+        cout << res << endl;
+        // cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
